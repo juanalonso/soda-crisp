b5cff5ab9711d2d6e515f94c048c8373475e4573
Juan Alonso <juan@neuronasmuertas.com>
2020-11-18 16:14:31 +0100

    Dropping json skips one step

--- js/index.js
+++ js/index.js
@@ -29,8 +29,9 @@ function runExperiment() {
 
 function gotFile(file) {
     experiment = file.data;
-    runexp.html('Run ' + experiment.expname + '<br\><small>' + experiment.measname + '</small>');
-    runexp.removeClass('hidden')
+    //runexp.html('Run ' + experiment.expname + '<br\><small>' + experiment.measname + '</small>');
+    //runexp.removeClass('hidden')
+    window.open('run.html', '_self');
 }
 
 function highlight() {
